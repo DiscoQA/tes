03b1a50233903e45ebd94c1ab24835f32fb7876d
andrey.goncharov <asgonch91@gmail.com>
2024-09-26 12:45:37 +0300

    ;fvl

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM python:3.9.20-slim-bullseye
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
 WORKDIR /app
 # copy project
+
 COPY . /app
 # install dependencies
+RUN pip install --no-cache -r /app/requirements.txt
+
 # run app
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # This is a sample Python script.
+from environs import Env
+env = Env()
+env.read_env()
+code1: str = env('my_test_code')
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -7,7 +12,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'pidooor, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print (code1)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
